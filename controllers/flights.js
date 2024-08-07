e29845018046a733a9e0a8329c368271ff67e891
Sunny Master <master.codeworks@gmail.com>
2024-08-07 13:46:03 -0400

    add update functionality for flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -52,10 +52,35 @@ async function deleteFlight(req, res) {
   }
 }
 
+async function edit(req, res) {
+  try {
+    const flight = await Flight.findById(req.params.flightId)
+    res.render('flights/edit', {
+      title: 'Edit Flight',
+      flight
+    })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/flights')
+  }
+}
+
+async function update(req, res) {
+  try {
+    const flight = await Flight.findByIdAndUpdate(req.params.flightId, req.body, {new: true})
+    res.redirect(`/flights/${flight._id}`)
+  } catch (error) {
+    console.log(error)
+    res.redirect(`/flights/${req.params.flightId}`)
+  }
+}
+
 export {
   newFlight as new,
   create,
   index,
   show,
   deleteFlight as delete,
+  edit,
+  update,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -12,10 +12,16 @@ router.get('/new', flightsCtrl.new)
 // GET /flights/:flightId
 router.get('/:flightId', flightsCtrl.show)
 
+//GET /flights/:flightId/edit
+router.get('/:flightId/edit', flightsCtrl.edit)
+
 // POST /flights
 router.post('/', flightsCtrl.create)
 
 // DELETE /flights/:flightId
 router.delete('/:flightId', flightsCtrl.delete)
+
+// PUT /flights/:flightId
+router.put('/:flightId', flightsCtrl.update)
 
 export { router }
