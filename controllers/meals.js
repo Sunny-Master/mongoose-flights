9192690447ca4e340985b01cc461ac0a2335cb3b
Sunny Master <master.codeworks@gmail.com>
2024-08-08 20:09:57 -0400

    add a display message on add meal page for when a meal already exists

--- controllers/meals.js
+++ controllers/meals.js
@@ -4,12 +4,27 @@ async function newMeal(req, res){
   const meals = await Meal.find({})
   res.render('meals/new', {
     title: 'Add Meal',
-    meals
+    meals,
+    mealExists: false,
   })
 }
 
 async function create(req, res) {
   try {
+    const meals = await Meal.find({})
+    let mealExists = false
+    meals.forEach(meal => {
+      if (meal.name === req.body.name) {
+        mealExists = true
+      }
+    })
+    if (mealExists) {
+      return res.render('meals/new', {
+        title: 'Add Meal',
+        meals,
+        mealExists
+      })
+    }
     const meal = await Meal.create(req.body)
     res.redirect('/meals/new')
   } catch (error) {
